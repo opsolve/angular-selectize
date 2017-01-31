a7d2658dab368de68a8354b17c8887572c2936a4
OpSolve LLC <lectus@opsolve.com>
2017-01-31 18:48:42 -0200

    Update angular-selectize.js

--- dist/angular-selectize.js
+++ dist/angular-selectize.js
@@ -117,15 +117,17 @@ angular.module("selectize", []).value("selectizeConfig", {}).directive("selectiz
             }
 
             config.onChange = function () {
-                var selectizeDependents = angular.element(document).find("[data-dependent=" + attrs.name + "]");
+                if (element[0].selectize.items.length > 0) {
+                    var selectizeDependents = angular.element(document).find("[data-dependent=" + attrs.name + "]");
 
-                angular.forEach(selectizeDependents, function (dependent) {
-                    if (dependent.selectize.isDisabled === false) {
-                        dependent.selectize.disable();
-                        dependent.selectize.clear();
-                        angular.element(dependent).attr("data-locked-by", attrs.name);
-                    }
-                });
+                    angular.forEach(selectizeDependents, function (dependent) {
+                        if (dependent.selectize.isDisabled === false) {
+                            dependent.selectize.disable();
+                            dependent.selectize.clear();
+                            angular.element(dependent).attr("data-locked-by", attrs.name);
+                        }
+                    });
+                }
 
                 if (scope.disableOnChange)
                     return;
